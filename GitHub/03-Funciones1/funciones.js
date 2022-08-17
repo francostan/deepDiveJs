be547fe0f9734e93c43cf0a006581e1b66d2088b
francostan <francostan98@hotmail.com>
2022-08-16 21:32:44 -0300

    funciones 03 listas
    hicimos una funcion que funciona como calculadora, donde retorna un objeto que tiene como atributo otra funcion, la cual debera realizar una accion segun corresponda

--- GitHub/03-Funciones1/funciones.js
+++ GitHub/03-Funciones1/funciones.js
@@ -1,3 +1,17 @@
-function hola(){
-    console.log("Hola");
+function crearCalculadora() {
+  var valor = 0;
+  return {
+    valor: function() {
+      return valor;
+    },
+    sumar: function(n) {
+      valor += n;
+    },
+    restar: function(n) {
+      valor -= n;
+    },
+    reset: function() {
+      valor = 0;
+    }
+  };
 }

6401ceff7c9c07b8d3252216b46bd088458f062b
francostan <francostan98@hotmail.com>
2022-08-18 19:53:57 -0300

    hecho hasta creador de incrementos
    falta

--- GitHub/04-Funciones2/funciones.js
+++ GitHub/04-Funciones2/funciones.js
@@ -1,6 +1,21 @@
 function concatenar (){
-    return arguments.join('');
+    var args = Array.prototype.slice.call(arguments);
+    return args.join("");
 }
-function invocandoFunciones(){
-    return arguments.map(function(funcion){}).join('');
+function invocarFunciones(){
+    var args = Array.prototype.slice.call(arguments);
+    var result = "";
+    for(var i = 0; i < args.length; i++){
+        result += args[i]();
+    }
+    return result;
 }
+function creadorDeIncrementos(incremento) {
+    return function(nombre) {
+        return nombre + incremento;
+    }
+  }
+invocacionUnica = function(nombre){
+    return nombre + "1";
+}
+

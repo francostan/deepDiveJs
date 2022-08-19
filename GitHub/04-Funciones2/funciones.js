c72701fbb09a75e07e95e1ff2449d65ce75cfd12
francostan <francostan98@hotmail.com>
2022-08-19 12:42:07 -0300

    me quedaron los ultimos 3 tests

--- GitHub/04-Funciones2/funciones.js
+++ GitHub/04-Funciones2/funciones.js
@@ -11,11 +11,57 @@ function invocarFunciones(){
     return result;
 }
 function creadorDeIncrementos(incremento) {
-    return function(nombre) {
-        return nombre + incremento;
+    return function(num) {
+        return num + incremento;
     }
   }
-invocacionUnica = function(nombre){
-    return nombre + "1";
-}
 
+  function invocacionUnica(funcion) {
+    let invocado = false;
+    return function() {
+        if(invocado) {
+            return;
+        }
+        //dentro de este scope ya que seteado invocado como true
+        invocado = true;
+        return funcion.call(this, arguments);
+    }
+  }
+  function objetoConClousure() {
+    let obj={
+        incremento: creadorDeIncrementos(1),
+        incrementoPor10: creadorDeIncrementos(10),
+        pedirValor: function() {
+            return this.valor;
+        },
+        cambiarValor: function(nuevoValor) {
+            this.valor = nuevoValor;
+        }
+    }
+    return obj;
+  }
+  function ListaDeFuncionesInvitados(arregloInvitados, codigoSecreto) {
+    var funciones = [];
+    for(var i = 0; i < arregloInvitados.length; i++) {
+        funciones.push(function(codigo) {
+            if(codigo === codigoSecreto) {
+                return arregloInvitados[i];
+            }
+            return "código secreto: invalido";
+        });
+    }
+    return funciones;
+  }
+
+  function armarListaDeInvitados (a,b){
+    let array= [];
+
+    for(let i = 0; i < a.length; i++){
+        if(b === 404) {
+            array.push(a[i].toString());
+        }else{         
+        console.log( "código secreto: invalido");
+        }
+    };
+    return array;
+  }
